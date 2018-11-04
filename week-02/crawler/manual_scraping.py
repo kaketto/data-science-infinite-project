b85acebc1f78c5387eea9e1d51f302e4df455b77
Kinga Moldovanyi <kinga.mold@gmail.com>
2018-11-04 18:26:05 +0100

    fix(manual scraping): unique links

--- week-02/crawler/manual_scraping.py
+++ week-02/crawler/manual_scraping.py
@@ -12,16 +12,18 @@ url = "http://www.jegkorongblog.hu/"
 def get_article_links():
     response = requests.get(url)
     soup = bs.BeautifulSoup(response.text, "lxml")
-    pattern = url + r'2018\/\d{2}\/\d{2}\/\w+(-\w*)*\/#post-\d{6}'
-    article_links = soup.find_all(href=re.compile(pattern))
-    return article_links
+    pattern = url + r'\d{4}\/\d{2}\/\d{2}\/\w+(-\w*)*\/#post-\d{6}'
+    a_tags = soup.find_all(href=re.compile(pattern))
+    article_links = [item['href'] for item in a_tags]
+    unique_article_links = list(set(article_links))
+    return unique_article_links
 
 def get_article_text():
     articles = get_article_links()
     titles = []
     texts = []
     for article in articles:
-        response = requests.get(article['href'])
+        response = requests.get(article)
         soup = bs.BeautifulSoup(response.text, "lxml")
         title = soup.h1.find(text=True, recursive=False)
         titles.append(title)
@@ -29,7 +31,7 @@ def get_article_text():
         text = [p.text for p in p_tags]
         article_text = ' '.join(text)
         texts.append(article_text)
-    icehockey_articles = {'title': titles, 'text': texts}
+    icehockey_articles = {'title': titles, 'link': articles, 'text': texts}
     df = pd.DataFrame(icehockey_articles, columns = icehockey_articles.keys())
     df.to_csv('jegkorongblog.csv') 
     return df
